511479779a87b53784ea0e785dc4ce3390ee8d6c
Vikash Kushwaha <alfredgerogiannis8@gmail.com>
2025-11-19 01:59:51 -0500

    main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,39 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
-```
-
-2. **Create `client/src/App.tsx`:**
-```typescript
-import { Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Profile from './pages/Profile'
-import Posts from './pages/Posts'
-import CreatePost from './pages/CreatePost'
-import ProtectedRoute from './components/ProtectedRoute'
-
-function App() {
-  return (
-    <AuthProvider>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/create-post" element={<ProtectedRoute><CreatePost /></ProtectedRoute>} />
-            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          </Routes>
-        </main>
-      </div>
-    </AuthProvider>
-  )
-}
-
-export default App
